013418866c9a3f4f6b00ae1d1f40c4b592f1c4af
luisa_gutierrez <luisa.gutierrez97@misena.edu.co>
2022-11-04 10:53:37 -0500

    modificado modelos quotes

--- quotes/models.py
+++ quotes/models.py
@@ -8,3 +8,7 @@ class Quotes(models.Model):
     Time = models.TimeField()
     Img = models.ImageField()
     Description = models.CharField(max_length=150, blank=False, default='')
+
+
+# Tattoo_artist = models.ForeignKey(Tattoo_artist, on_delete=models.CASCADE)
+# User = models.ForeignKey(Users, on_delete=models.CASCADE)
