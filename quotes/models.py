b44bd217005e17bf42962e1decad2045c7f135fe
luisa_gutierrez <luisa.gutierrez97@misena.edu.co>
2022-11-29 06:32:12 -0500

    ST se modifico img citas

--- quotes/models.py
+++ quotes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
+# from accounts import models 
 
 
 # Create your models here.
@@ -12,7 +13,7 @@ class Quotes(models.Model):
     id_quotes = models.BigAutoField(primary_key=True)
     date = models.DateField()
     time = models.TimeField()
-    img = models.CharField(max_length=100)
+    img = models.ImageField(max_length=100)
     description = models.CharField(max_length=150, blank=False, default='')
     userID = models.ForeignKey(Users, on_delete=models.CASCADE, related_name="perfilUser")
     artist_tattoo = models.ForeignKey(Users, on_delete=models.CASCADE, related_name= "perfilTattoo")
